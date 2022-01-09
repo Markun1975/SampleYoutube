9b8e78c6d0809f60b7e3635f4f60ec6b07ee5f1a
= <=>
2022-01-10 07:10:27 +0900

    Initial Commit

---
+++ SampleYoutube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleYoutube
+//
+//  Created by Masaki on R 4/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
