6c59e37aedaf74f5e660a13a0a0765f2a1887af2
Markun1975 <choco_9031@yahoo.co.jp>
2022-01-24 23:56:54 +0900

    IBOutlet追加修正

--- SampleYoutube/Views/Collections/Cells/VideoTableViewCell.swift
+++ SampleYoutube/Views/Collections/Cells/VideoTableViewCell.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class VideoTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var thumbnailImageView: UIView!
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var supplementLabel: UILabel!
+    static var reuseIdentifier: String { return String(describing: self) }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
