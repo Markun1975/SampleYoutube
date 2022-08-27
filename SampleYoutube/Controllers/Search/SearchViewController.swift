75bdc817d3609ea4daad2b9e5bc802c49ac66f5b
Markun1975 <choco_9031@yahoo.co.jp>
2022-08-27 15:07:30 +0900

    SearchViewController新規追加

---
+++ SampleYoutube/Controllers/Search/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  SampleYoutube
+//
+//  Created by Masaki on R 4/07/03.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .yellow
+    }
+
+}
